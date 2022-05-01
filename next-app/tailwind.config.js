ea1d5ef61456f73cfb4a716d7eb0ed1605fcfa5a
leo6liu <leo.battalora@gmail.com>
2022-04-30 20:09:47 -0400

    extend rather than replcae color and font themes

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -1,25 +1,22 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: "#000",
-      white: "#fff",
-      "space-cadet": "#22223B",
-      indepencence: "#4A4E69",
-      "heliotrope-gray": "#9A8C98",
-      isabelline: "#F2E9E4",
-      "raw-sienna": "#D67941",
+    extend: {
+      colors: {
+        "space-cadet": "#22223B",
+        indepencence: "#4A4E69",
+        "heliotrope-gray": "#9A8C98",
+        isabelline: "#F2E9E4",
+        "raw-sienna": "#D67941",
+      },
+      fontFamily: {
+        "rampart-one": ["Rampart One"],
+        "new-tegomin": ["New Tegomin"],
+        sans: ["Noto Sans JP"],
+        serif: ["Noto Serif JP"],
+        "shippori-mincho": ["Shippori Mincho"],
+      },
     },
-    fontFamily: {
-      "rampart-one": ["Rampart One"],
-      "new-tegomin": ["New Tegomin"],
-      "sans": ["Noto Sans JP"],
-      "serif": ["Noto Serif JP"],
-      "shippori-mincho": ["Shippori Mincho"],
-    },
-    extend: {},
   },
   plugins: [],
 };
