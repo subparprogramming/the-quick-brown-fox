5764f1279aaf2e4fc7081d8bb3b1032a685a4837
leo6liu <leo.battalora@gmail.com>
2022-05-01 01:53:06 -0400

    text-size and padding

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -16,15 +16,15 @@ const Home: NextPage = () => {
         {/* contents */}
         <div className="max-w-screen-xl">
           {/* header */}
-          <div className="grid grid-flow-row grid-cols-3 items-center px-8">
-            <div className="col-span-3 flex flex-col justify-start space-x-2 p-4 font-rampart-one text-8xl font-bold sm:col-span-2">
+          <div className="grid grid-flow-row grid-cols-3 items-center p-4 sm:p-8">
+            <div className="col-span-3 flex flex-col justify-start space-x-2 font-rampart-one text-7xl font-bold sm:col-span-2 sm:text-8xl xl:text-9xl">
               <div className="flex flex-wrap">
                 <h1 className="">The</h1>
                 <h1 className="">Quick</h1>
                 <h1 className="">Brown</h1>
                 <h1 className="">Fox</h1>
               </div>
-              <h1 className="pl-2 pt-4 text-3xl">おはよう!!!</h1>
+              <h1 className="pl-1 pt-4 text-3xl">おはよう!!!</h1>
             </div>
             <img src="/fox-animated-1.gif" className="col-span-2 h-1/2 w-full object-cover object-left-bottom sm:col-span-1" style={{ imageRendering: "pixelated" }} />
           </div>
