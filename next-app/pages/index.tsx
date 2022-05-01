87f12f8dbea008e05f727a9ef0312149a1bd8517
leo6liu <leo.battalora@gmail.com>
2022-04-30 20:50:31 -0400

    display text and dog

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -10,12 +11,18 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="min-h-screen bg-space-cadet font-rampart-one text-4xl font-bold text-isabelline">
-        <h1 className="font-rampart-one">The</h1>
-        <h1 className="font-new-tegomin">Quick</h1>
-        <h1 className="font-sans">Brown</h1>
-        <h1 className="font-serif">Fox</h1>
-        <h1 className="font-shippori-mincho">おはよう</h1>
+      <div className="flex min-h-screen w-full flex-col bg-space-cadet text-isabelline">
+        <div className="grid grid-flow-row grid-cols-3 items-center px-8">
+          <div className="col-span-2 flex flex-wrap space-x-2 p-4 font-rampart-one text-8xl font-bold">
+            <h1 className="">The</h1>
+            <h1 className="">Quick</h1>
+            <h1 className="">Brown</h1>
+            <h1 className="">Fox</h1>
+            <h1 className="pl-4 pt-8 text-3xl">おはよう!!!</h1>
+          </div>
+
+          <img src="/fox-animated-1.gif" className="h-1/2 w-full object-cover object-bottom" style={{ imageRendering: "pixelated" }} />
+        </div>
       </div>
     </>
   );
