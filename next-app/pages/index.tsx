4d693af5d6961f08c919c01037bce42ac96e970f
leo6liu <leo.battalora@gmail.com>
2022-05-19 17:38:03 -0400

    text changes

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -36,17 +36,17 @@ const Home: NextPage = () => {
           {/* learn hiragana/katakana buttons */}
           <div className="mx-auto grid w-full grid-flow-row grid-cols-1 justify-items-center gap-4 px-8 py-2 md:w-3/4 md:grid-cols-2">
             <Link href="/hiragana">
-              <div className="col-span-1 flex w-full justify-center rounded-lg border-2 border-independence bg-independence p-2 duration-200 hover:border-raw-sienna">Learn Hiragana</div>
+              <div className="col-span-1 flex w-full justify-center rounded-lg border-2 border-independence bg-independence p-2 duration-200 hover:border-raw-sienna">Hiragana Chart</div>
             </Link>
 
             <Link href="/katakana">
-              <div className="col-span-1 flex w-full justify-center rounded-lg border-2 border-independence bg-independence p-2 duration-200 hover:border-raw-sienna">Learn Katakana</div>
+              <div className="col-span-1 flex w-full justify-center rounded-lg border-2 border-independence bg-independence p-2 duration-200 hover:border-raw-sienna">Katakana Chart</div>
             </Link>
           </div>
 
           {/* grid content */}
           <div className="mx-auto mt-8 flex w-full flex-col md:w-3/4">
-            <div className="ml-auto mr-0 px-10 text-right">idk man... this doesn&apos;t really do sh*t 😑</div>
+            <div className="ml-auto mr-0 px-10 text-right">idk man... this part doesn&apos;t really do sh*t 😑</div>
             <div className="grid grid-flow-col grid-cols-5 grid-rows-4 px-8 pt-1 font-sans font-bold text-black">
               <div className="row-span-3 row-start-2 m-1 flex items-center justify-center rounded-lg bg-isabelline p-4">01</div>
               <div className="m-1 flex items-center justify-center rounded-lg bg-isabelline p-4">02</div>
